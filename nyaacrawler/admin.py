4d00b6c342358bf23b0ae3afa8f12e28d6fd4201
winston wu <winxton@gmail.com>
2013-08-17 16:39:02 -0700

    added user and subscription tables, moved webcrawler to utility function

--- nyaacrawler/admin.py
+++ nyaacrawler/admin.py
@@ -3,13 +3,25 @@ from nyaacrawler import models
 
 class AnimeAdmin(admin.ModelAdmin):
 	list_display = ('identifier','title')
+
 class AnimeAliasAdmin(admin.ModelAdmin):
     list_display = ('aliasName', "anime")
+
 class TorrentAdmin(admin.ModelAdmin):
     list_display = ('episode','fansub')
-    
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('email','created')
+
+class SubscriptionAdmin(admin.ModelAdmin):
+	list_display = ('user', 'anime')
+
 admin.site.register(models.Anime, AnimeAdmin)
 
 admin.site.register(models.AnimeAlias, AnimeAliasAdmin)
 
 admin.site.register(models.Torrent, TorrentAdmin)
+
+admin.site.register(models.User, UserAdmin)
+
+admin.site.register(models.Subscription, SubscriptionAdmin)
--- nyaacrawler/models.py
+++ nyaacrawler/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 from django.db.models.aggregates import Max
+
 # Create your models here.
 
 class Anime(models.Model):
 	identifier = models.IntegerField()
 	title = models.CharField(max_length=200)
-
 	def __unicode__(self):
 		return self.title
 	def latest_episodes(self):
 		return self.torrents.all().filter(episode=self.current_episode()['max_episode'])
 	def current_episode(self):
+		#ex: ["max_episode" : num]
 		return self.torrents.aggregate(max_episode=Max('episode'))
 
 class AnimeAlias(models.Model):
 	anime = models.ForeignKey(Anime, related_name="animeAliases")
 	aliasName = models.CharField(max_length=200)
+	def __unicode__(self):
+		return self.aliasName
 
 class Torrent(models.Model):
 	anime = models.ForeignKey(Anime, related_name='torrents')
@@ -27,3 +30,19 @@ class Torrent(models.Model):
 	tracker = models.CharField(max_length=40)
 	infoHash = models.CharField(max_length=30)
 	vidFormat = models.CharField(max_length=10)
+	def __unicode__(self):
+		return self.torrentName
+
+class User(models.Model):
+	email = models.EmailField()
+	created = models.DateTimeField(auto_now_add=True)
+	activationKey = models.CharField(max_length=30)
+	def __unicode__(self):
+		return self.email
+
+class Subscription(models.Model):
+	user = models.ForeignKey(User)
+	anime = models.ForeignKey(Anime)
+	current_episode = models.FloatField()
+	qualities = models.CharField(max_length=30)
+	subGroups = models.CharField(max_length=80)
